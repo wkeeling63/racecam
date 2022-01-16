a755b6e2050f33501e3b837c2809d02f8f9f943d
William Keeling <williamkeeling@hotmail.com>
2022-01-16 09:10:46 -0600

    testing xwindows

--- racecam.c
+++ racecam.c
@@ -330,7 +330,7 @@ int write_parms(char *mode, size_t size, void *ptr)
 void *record_thread(void *argp)
 {
   log_debug("%s in file: %s(%d)", __func__,  __FILE__, __LINE__);
-/*  
+  
   int file_selected = 0, url_selected = 0;
 
 //  if (global_state.selected[FILE_STRM])
@@ -373,7 +373,7 @@ void *record_thread(void *argp)
 		goto err_alsa;
 		}
 
-  if (create_video_stream(&global_state)) 
+/*  if (create_video_stream(&global_state)) 
 		{
 		goto err_vstream;
 		}
@@ -403,6 +403,7 @@ void *record_thread(void *argp)
 		global_state.adjust_q_state.min_q = global_state.quantisationMin;
 		pthread_create(&adjq_tid, NULL, adjust_q, (void *)&global_state.adjust_q_state);
 		}  
+    */
 
 	int64_t start_time = get_microseconds64()/1000;
 	
@@ -412,8 +413,8 @@ void *record_thread(void *argp)
   snd_pcm_prepare(global_state.pcmhnd);
   snd_pcm_start(global_state.pcmhnd);
   global_state.sample_cnt = 0;
-	mmal_port_parameter_set_boolean(global_state.camera_component[MAIN_CAMERA]->output[MMAL_CAMERA_VIDEO_PORT], MMAL_PARAMETER_CAPTURE, START);
-	mmal_port_parameter_set_boolean(global_state.camera_component[OVERLAY_CAMERA]->output[MMAL_CAMERA_VIDEO_PORT], MMAL_PARAMETER_CAPTURE, START);
+//	mmal_port_parameter_set_boolean(global_state.camera_component[MAIN_CAMERA]->output[MMAL_CAMERA_VIDEO_PORT], MMAL_PARAMETER_CAPTURE, START);
+//	mmal_port_parameter_set_boolean(global_state.camera_component[OVERLAY_CAMERA]->output[MMAL_CAMERA_VIDEO_PORT], MMAL_PARAMETER_CAPTURE, START);
 	
 	while (global_state.current_mode > 0 ) 
 		{
@@ -421,14 +422,14 @@ void *record_thread(void *argp)
     check_output_status(&global_state);
 		}
     
-  mmal_port_parameter_set_boolean(global_state.camera_component[MAIN_CAMERA]->output[MMAL_CAMERA_VIDEO_PORT], MMAL_PARAMETER_CAPTURE, STOP);
-	mmal_port_parameter_set_boolean(global_state.camera_component[OVERLAY_CAMERA]->output[MMAL_CAMERA_VIDEO_PORT], MMAL_PARAMETER_CAPTURE, STOP);
+//  mmal_port_parameter_set_boolean(global_state.camera_component[MAIN_CAMERA]->output[MMAL_CAMERA_VIDEO_PORT], MMAL_PARAMETER_CAPTURE, STOP);
+//	mmal_port_parameter_set_boolean(global_state.camera_component[OVERLAY_CAMERA]->output[MMAL_CAMERA_VIDEO_PORT], MMAL_PARAMETER_CAPTURE, STOP);
 	
   encode_queue_audio(&global_state, TRUE); // flush audio encoder
 	
 			
 err_vstream:
-  destroy_video_stream(&global_state);
+/*  destroy_video_stream(&global_state);
 	
   if (global_state.output_state[FILE_STRM].queue)
 		{
@@ -442,7 +443,7 @@ err_vstream:
 		{
 	  if (queue_end(global_state.output_state[URL_STRM].queue)) {log_error("End queue url stream failed");}
     if (global_state.output_state[URL_STRM].run_state == WRITING) global_state.output_state[URL_STRM].run_state = STOPPING_WRITE;
-		}
+		} */
 		
 err_alsa:
   free_alsa(&global_state);
@@ -452,7 +453,7 @@ err_aencode:
 
 //  if (global_state.selected[FILE_STRM]) 
 //  if (global_state.output_state[FILE_STRM].run_state)
-  if (file_selected) 
+/*  if (file_selected) 
 		{
 		pthread_join(file_tid, NULL);
 		}  
